3f3d8a1eed04b32b890e3c0c1839660c34cd8b5d
Dejan Zlataric <d.zlataric@levi9.com>
2020-07-05 13:53:03 +0200

    init

---
+++ es/src/test/java/rs/sc/base/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.sc.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
